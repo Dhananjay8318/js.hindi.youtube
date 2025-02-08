6ca3dd056d233e538faea2f2278890003e1a1789
Dhananjay8318 <dhananjay.23b0131059@abes.ac.in>
2025-02-08 05:49:45 +0000

    container config done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const accountId=83182569
+let accountEmail="dhananjay@gmail.com"
+var accountPassword="44512165668"
+accountCity ="ghazipur"
+let accountState
+
+accountEmail="dhananjaydj94500@google.com"
+accountPassword="12123456"
+accountCity="varanasi"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*
+not use var because of in issue in block scope and function scope
+*/ 
