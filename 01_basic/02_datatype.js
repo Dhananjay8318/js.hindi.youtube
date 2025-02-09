12cdf6048b84b392db9d04b68987f496b570382e
Dhananjay8318 <dhananjay.23b0131059@abes.ac.in>
2025-02-09 08:13:10 +0000

    contaiiner config done

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all code in a js as new version
+//alert(3+3)  here using node js not a browser
+let name ="Dhananjay"
+let age=20
+let idLoggedIn=false
+
+
+//number
+//boolean
+//string
+//null= standard value
+//undefined
+//symbol = talk for unique
+//object
+
+console.log(typeof age)
+console.log(typeof name)
