70c350930e0f89e685af6d86e2beecc277eab507
Dhananjay8318 <dhananjay.23b0131059@abes.ac.in>
2025-02-10 10:28:29 +0000

    strings

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,16 @@
+const name= "dhananjay"
+const repo=8
+console.log(`hello my name is $ {name} and my repo is ${repo}`);
+// console.log(name+repo)
+const gameName=new String("Dhananjay")
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(3))
+console.log(gameName.indexOf("j"))
+const newString = gameName.substring(0,5)
+console.log(newString)
+
+
+const newStringOne="   Dhananjay     "
+console.log(newStringOne)    //it give a space
+console.log(newStringOne.trim())// it remove space
